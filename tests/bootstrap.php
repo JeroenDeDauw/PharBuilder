4bd9df5d19ca4a89b933a885c4ab96a19e03b4c9
jeroendedauw <jeroendedauw@gmail.com>
2013-06-18 17:29:08 +0200

    Added phpunit config file and namespaces

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * PHPUnit bootstrap file for PharBuilder.
+ *
+ * @since 0.1
+ *
+ * @file
+ * @ingroup PharBuilder
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
